4c37787d9538abee9dca3bbb263a8cd4f657597b
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-08-12 09:41:44 +0530

    Dhanraj Mon

--- Node_js_api/models/user.model.js
+++ Node_js_api/models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = mongoose.Schema({
   birthdate: { type: Date, required: true },
   gender: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   
 }, {
